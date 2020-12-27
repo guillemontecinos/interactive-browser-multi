60d3ca7e1f7f8bfe08032e89cdeada89be718842
gamontec <gamontec@uc.cl>
2020-12-26 21:20:18 -0300

    sending notes from multiple clients throught admin via midi messages

--- app/public/admin.js
+++ app/public/admin.js
@@ -22,7 +22,8 @@ socket.on('new client to admin', function(data){
     container.appendChild(sketch)
     document.body.appendChild(container)
     // saves the data in the clients array
-    clients.push({id: data.id, username: data.username, instance: new p5(s, document.getElementById(data.id)), shape: []})
+    // TODO: channel has to be setup based on the user's decision, this is just for testing
+    clients.push({id: data.id, username: data.username, instance: new p5(s, document.getElementById(data.id)), shape: [], channel: clients.length + 1})
     // console.log(clients)
 })
 
@@ -73,10 +74,10 @@ const s = function(sketch){
 }
 
 // MIDI section
-// Assuming time signature = 4/4
 let ppqnCount = 0
 let barCount = 1
 let octaves = 1
+let timeNumerator = 4, timeDenominator = 4
 
 WebMidi.enable(function (err) {
     // Adds inputs to the dropdown menu when new ports connect
@@ -98,10 +99,11 @@ WebMidi.enable(function (err) {
         })
         WebMidi.getInputByName('auxVirtualPort Bus 1').addListener('clock', 'all', (e) => {
             ppqnCount++
-            if(ppqnCount == 0 || ppqnCount == 24){
+            const ppqnComparator = 24 * 4 / timeDenominator
+            if(ppqnCount == 0 || ppqnCount == ppqnComparator){
                 ppqnCount = 0
                 barCount++
-                if(barCount == 5) barCount = 1
+                if(barCount == timeNumerator + 1) barCount = 1
                 console.log('bar: ' + barCount)
                 // calculate and send notes
                 sendNote(barCount)
@@ -113,33 +115,41 @@ WebMidi.enable(function (err) {
 function sendNote(beat){
     if(clients.length > 0){
         clients.forEach(element => {
+            // TODO: implemente sending each user to a different channel
+            const numNotes = octaves * 12
+            const noteWidth = element.instance.int(element.instance.width / timeNumerator)
+            const noteHeight = element.instance.int(element.instance.height / numNotes)
+            
             // element.instance.fill(255,0,0,50)
             // element.instance.noStroke()
-            // element.instance.rect((beat - 1) * element.instance.width / 4, 0, element.instance.width / 4, element.instance.height)
-            // TODO: instead of drawing the rectangle, estimate density on each square depending on how many octaves were set by the user
-            let numNotes = octaves * 12
-            let noteWidth = element.instance.width / 4
-            let noteHeight = element.instance.height / numNotes
-            let notes = []
+            // element.instance.rect((beat - 1) * noteWidth, 0, noteWidth, element.instance.height)
+
             element.instance.loadPixels()
             for (let i = 0; i < numNotes; i++){
-                // pixelsDensity
+                // Estimate pixelsDensity
                 let counter = 0, brightness = 0
-                for(let x = (beat - 1) * noteWidth; x < beat * noteWidth; x++){
+                for(let x = (beat - 1) * noteWidth; x < beat * noteWidth; x += 4){
                     if(x > element.instance.width) break
-                    for(let y = i; y < i + noteHeight; y++){
+                    for(let y = i * noteHeight; y < (i + 1) * noteHeight; y += 4){
                         if(y > element.instance.height) break
-                        let c = element.instance.get(x, y)
-                        brightness += element.instance.brightness(c)
+                        const c = element.instance.get(x, y)
+                        brightness += (element.instance.red(c) + element.instance.green(c) + element.instance.blue(c)) / 3
                         counter++
                     }
                 }
                 brightness /= counter
-                // console.log(brightness)
-                notes.push(element.instance.map(brightness, 0, 255, 127, 0))
-            }
-            // element.instance.updatePixels()
-            console.log(notes)
+                const vel = element.instance.map(brightness, 0, 255, 1, 0)
+                if(vel > .3){
+                    // Send note when avg is higher than some threshold
+                    WebMidi.getOutputByName('auxVirtualPort Bus 1').playNote(
+                        60 + numNotes - i, 
+                        element.channel, 
+                        {
+                            duration: 20, 
+                            velocity: vel
+                        })
+                    }
+                }
         })
     }
 }
